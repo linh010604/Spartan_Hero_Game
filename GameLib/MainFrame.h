8a6f8a61da91bf64f5b63bf2481b29c29bb4e54b
kaurrosh1 <kaurrosh@msu.edu>
2024-02-20 17:16:30 -0500

    initalized the mainframe class, the window is not opening yet becasue there needs to be changes made to the GameView class

---
+++ GameLib/MainFrame.h
@@ -0,0 +1,24 @@
+/**
+ * @file MainFrame.h
+ * @author Roshni Kaur
+ *
+ *
+ */
+#include <wx/wxprec.h>
+#include <wx/wx.h>
+#ifndef PROJECT1_GAMELIB_MAINFRAME_H
+#define PROJECT1_GAMELIB_MAINFRAME_H
+
+/**
+ * The top-level (main) frame of the application
+ */
+class MainFrame : public wxFrame {
+private:
+
+public:
+    void Initialize();
+
+
+};
+
+#endif //PROJECT1_GAMELIB_MAINFRAME_H

61d07f89c1e1fe975a6ee58b54a0e60077a5a0bd
Nico Roberts <93745296+nicocodez@users.noreply.github.com>
2024-02-20 10:50:33 -0500

    Added GameApp class, waiting for MainFrame implementation

---
+++ GameApp.h
@@ -0,0 +1,21 @@
+/**
+ * @file GameApp.h
+ * @author Nicolas Roberts
+ *
+ * Main application class
+ */
+
+#ifndef PROJECT1_GAMEAPP_H
+#define PROJECT1_GAMEAPP_H
+
+/**
+ * Main application class
+ */
+class GameApp : public wxApp {
+public:
+    bool OnInit() override;
+
+};
+
+
+#endif //PROJECT1_GAMEAPP_H

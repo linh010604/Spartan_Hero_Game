99d2a1c93684dd01baf8b21c64a89a927f22464c
kaurrosh1 <kaurrosh@msu.edu>
2024-02-21 17:19:38 -0500

    added level class

---
+++ GameLib/Level.h
@@ -0,0 +1,19 @@
+/**
+ * @file Level.h
+ * @author Roshni Kaur
+ *
+ *
+ */
+
+#ifndef PROJECT1_GAMELIB_LEVEL_H
+#define PROJECT1_GAMELIB_LEVEL_H
+
+class Level
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_GAMELIB_LEVEL_H

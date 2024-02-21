f03994c949c255d2a5f7b0e2e62a09f06ac84239
kaurrosh1 <kaurrosh@msu.edu>
2024-02-21 17:24:27 -0500

    added level class

---
+++ GameLib/DeclarationImage.h
@@ -0,0 +1,19 @@
+/**
+ * @file DeclarationImage.h
+ * @author Roshni Kaur
+ *
+ *
+ */
+
+#ifndef PROJECT1_GAMELIB_DECLARATIONIMAGE_H
+#define PROJECT1_GAMELIB_DECLARATIONIMAGE_H
+
+class DeclarationImage
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_GAMELIB_DECLARATIONIMAGE_H
---
+++ GameLib/DeclarationItem.h
@@ -0,0 +1,19 @@
+/**
+ * @file DeclarationItem.h
+ * @author Roshni Kaur
+ *
+ *
+ */
+
+#ifndef PROJECT1_GAMELIB_DECLARATIONITEM_H
+#define PROJECT1_GAMELIB_DECLARATIONITEM_H
+
+class DeclarationItem
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_GAMELIB_DECLARATIONITEM_H

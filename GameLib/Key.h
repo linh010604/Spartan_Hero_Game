7e0a124e0a6b14069f65ebf67a511267c7e47fab
Nico Roberts <93745296+nicocodez@users.noreply.github.com>
2024-02-22 22:25:14 -0500

    Added Key class

--- GameLib/Key.h
+++ GameLib/Key.h
@@ -1,12 +1,16 @@
 /**
  * @file Key.h
  * @author Nicolas Roberts
+ *
+ * Main class for key
  */
 
 #ifndef PROJECT1_KEY_H
 #define PROJECT1_KEY_H
 
-
+/**
+ * Main class for key
+ */
 class Key {
 
 };

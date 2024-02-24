04fa2c31487c8caf73b82d3cdf6c5f4950c340f5
Nico Roberts <93745296+nicocodez@users.noreply.github.com>
2024-02-23 22:39:35 -0500

    Added Soundboard class

---
+++ GameLib/Soundboard.h
@@ -0,0 +1,15 @@
+/**
+ * @file Soundboard.h
+ * @author Nicolas Roberts
+ */
+
+#ifndef PROJECT1_SOUNDBOARD_H
+#define PROJECT1_SOUNDBOARD_H
+
+
+class Soundboard {
+
+};
+
+
+#endif //PROJECT1_SOUNDBOARD_H

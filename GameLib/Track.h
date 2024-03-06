422eb5d9012db0a0a7ec45d00ebf4879d91928c2
nguy1132 <nguy1132@msu.edu>
2024-03-06 17:42:33 -0500

    Update Track.h

--- GameLib/Track.h
+++ GameLib/Track.h
@@ -14,12 +14,65 @@
 #include <vector>
 
 /**
+ * Allows access to ItemSoundBoars without creating a circular dependency.
+ */
+class ItemSoundBoard;
+
+/**
  * Class for Track that derives from Item
  */
-class Track : public Item {
-public:
+class Track {
+private:
 
-//    Track(Game *game, const std::wstring &filename);
+    /// The soundboard this track is contained in
+    ItemSoundBoard  *mItemSoundBoard;
+
+    /// The track number
+    int mTrack = 0;
+
+    /// The bitmap we can display for this soundboard
+    std::unique_ptr<wxBitmap> mKeyBitmap;
+
+    wxString  mKey = "";  ///< Key of the track
+    double mWidth = 0;  ///< Width of the key
+    double mHeight = 0; ///< Length of the key
+    // Item location in the game
+    double  mX = 0;     ///< X location for the center of the item
+    double  mY = 0;     ///< Y location for the center of the item
+
+public:
+    /// Default constructor (disabled)
+    Track() = delete;
+
+    /// Copy constructor (disabled)
+    Track(const Track &) = delete;
+
+    /// Assignment operator
+    void operator=(const Track &) = delete;
+
+    Track(ItemSoundBoard *soundboard);
+
+    /**
+     * Set X location of the item
+     * @param x X position
+     */
+    void SetX(double x) { mX = x; }
+
+    /**
+     * Set Y location of the item
+     * @param y Y position
+     */
+    void SetY(double y) { mY = y; }
+
+    void XmlLoad(wxXmlNode *node);
+
+    /**
+     * Draw this item
+     * @param gp Device context to draw on
+     * @param x X position
+     * @param y Y position
+     */
+    void Draw(std::shared_ptr<wxGraphicsContext> gp, double x, double y);
 
 };
 

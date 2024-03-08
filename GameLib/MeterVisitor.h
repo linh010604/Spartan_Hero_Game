1d57698a088505fd39e6a3f000cb93dbb5e8979f
kaurrosh1 <kaurrosh@msu.edu>
2024-03-08 18:21:22 -0500

    added virtual function to MeterVisitor and SoundboardVisitor classes

--- GameLib/MeterVisitor.h
+++ GameLib/MeterVisitor.h
@@ -8,11 +8,26 @@
 #ifndef PROJECT1_GAMELIB_METERVISITOR_H
 #define PROJECT1_GAMELIB_METERVISITOR_H
 
-class MeterVisitor
-{
+class ItemSoundboard;
+
+class MeterVisitor{
+protected:
+    /**
+    * Constructor
+    * Ensures this is an abstract class
+    */
+
+
 private:
 
 public:
+    virtual ~MeterVisitor () {}
+    /**
+    * Visit soundboard
+    * @param soundboard The soundboard we are visiting
+    */
+    virtual void VisitSoundboard (ItemSoundboard* soundboard) {}
+
 
 };
 
--- GameLib/SoundBoardVisitor.h
+++ GameLib/SoundBoardVisitor.h
@@ -8,11 +8,28 @@
 #ifndef PROJECT1_GAMELIB_SOUNDBOARDVISITOR_H
 #define PROJECT1_GAMELIB_SOUNDBOARDVISITOR_H
 
-class SoundBoardVisitor
-{
+class ItemKey;
+
+class SoundBoardVisitor{
+
+protected:
+    /**
+     * Constructor
+     * Ensures this is an abstract class
+     */
+     SoundBoardVisitor() {}
+
 private:
 
 public:
+    virtual  ~SoundBoardVisitor() {}
+
+    /**
+    * Visit key
+    * @param key The key we are visiting
+    */
+
+    //virtual void VisitSoundboard (ItemKey* soundboard) {}
 
 };
 

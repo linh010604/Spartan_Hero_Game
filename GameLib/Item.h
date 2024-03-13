a6b9c11913594a511eec7e36dc864087a354656c
kaurrosh1 <kaurrosh@msu.edu>
2024-03-13 12:56:41 -0400

    added star image and fixed typo, added image to be loaded in level 3

--- GameLib/Item.h
+++ GameLib/Item.h
@@ -80,7 +80,7 @@ public:
 
     virtual void XmlLoad(wxXmlNode *node);
 
-    virtual void Draw(std::shared_ptr<wxGraphicsContext> gp, std::shared_ptr<Declaration> declaration, bool beforeSounfboard);
+    virtual void Draw(std::shared_ptr<wxGraphicsContext> gp, std::shared_ptr<Declaration> declaration, bool beforeSoundboard);
 
     /**
     * Accept a visitor
